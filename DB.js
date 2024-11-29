5bdaba53f0af655c036bf41e36160b75a353f96d
David Ndizeye <126089767+ndizeyedavid@users.noreply.github.com>
2024-11-29 12:52:39 +0200

    added port from env

--- DB.js
+++ DB.js
@@ -7,6 +7,7 @@ const db = mysql2.createConnection({
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
+  port: process.env.DB_PORT,
 });
 
 db.on("connect", () => {
