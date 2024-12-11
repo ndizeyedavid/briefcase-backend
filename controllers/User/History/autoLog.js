46851cc9bdff5d111b062a3089ac3f37c29b8786
David Ndizeye <126089767+ndizeyedavid@users.noreply.github.com>
2024-12-11 03:46:29 -0800

    Update autoLog.js

--- controllers/User/History/autoLog.js
+++ controllers/User/History/autoLog.js
@@ -3,7 +3,7 @@ import db from "../../../DB.js";
 const LogThis = (log) => {
   const sql = "INSERT INTO history(details) VALUES(?)";
   db.query(sql, [log], (err, result) => {
-    if (err) return console.log("Failed to save this log => '" + log + "'");
+    if (err) return console.log("Failed to save this log => '" + log + "' ERROR: "+err.message);
   });
 };
 
