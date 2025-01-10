75fbc86350c1a6cc4d88c83eb6aa89e678d8f537
David Ndizeye <126089767+ndizeyedavid@users.noreply.github.com>
2025-01-10 15:54:57 +0200

    SaveFailedAttempt configured on POST method

--- routers/board/BoardRouter.js
+++ routers/board/BoardRouter.js
@@ -3,6 +3,7 @@ import {
   fetchStatus,
   changeBriefcaseState,
   sendTrackingInfo,
+  SaveFailedAttempt
 } from "../../controllers/Board/BoardControllers.js";
 
 const BoardRouter = express.Router();
@@ -10,5 +11,5 @@ const BoardRouter = express.Router();
 BoardRouter.get("/briefcase/status", fetchStatus);
 BoardRouter.put("/briefcase/status/change", changeBriefcaseState);
 BoardRouter.post("/briefcase/tracking", sendTrackingInfo);
-
+BoardRouter.post("/briefcase/failed", SaveFailedAttempt);
 export default BoardRouter;
