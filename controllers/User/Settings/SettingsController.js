9cee8d2e9a89e52810f10c1d2d5041d962c1d77c
David Ndizeye <126089767+ndizeyedavid@users.noreply.github.com>
2025-01-10 16:27:06 +0200

    fix #5 failed attempt fetching

--- controllers/User/Settings/SettingsController.js
+++ controllers/User/Settings/SettingsController.js
@@ -52,7 +52,7 @@ const ResetField = (req, res) => {
 };
 
 const FailedAttempts = (req, res) => {
-  const sql = "SELECT * FROM notifications WHERE date=CURDATE() limit 1 order by date desc"
+  const sql = "SELECT * FROM notifications WHERE date = CURDATE() ORDER BY date DESC LIMIT 1"
   db.query(sql, (err, result)=>{
     if (err) return res.status(500).json({message: "unable to fetch failed attempts", error: err.message});
     res.status(200).json({result: result})
